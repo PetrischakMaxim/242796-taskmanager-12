52b0beaba8e80fbc1785ce2c36298693352cb187
Maksim Petrishchak <petrischak.maxim@gmail.com>
2020-08-04 18:55:08 +0300

    Вынес функции и константы в отдельные файлы

---
+++ src/utils.js
@@ -0,0 +1,24 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+export const isExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = new Date();
+
+  currentDate.setHours(23, 59, 59, 999);
+
+  return currentDate > dueDate.getTime();
+};
+
+export const isRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
