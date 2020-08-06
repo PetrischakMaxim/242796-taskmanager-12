b8b7fc8a3a84d7c56cfc244a5417c0da6451fb30
Maksim Petrishchak <petrischak.maxim@gmail.com>
2020-08-06 11:14:03 +0300

    Добавляю логику допоказа задач

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import {createPageMenuTemplate} from "./view/page-menu";
-import {createFilterTemplate} from "./view/filter";
-import {createTaskBoardTemplate} from "./view/task-board";
-import {createTaskCardTemplate} from "./view/card";
-import {createTaskEditCardTemplate} from "./view/card-edit";
-import {createLoadMoreButtonTemplate} from "./view/load-more-button";
-import {generateTask} from "./mock/task";
+import {createPageMenuTemplate} from "./view/page-menu.js";
+import {createFilterTemplate} from "./view/filter.js";
+import {createTaskBoardTemplate} from "./view/task-board.js";
+import {createTaskCardTemplate} from "./view/card.js";
+import {createTaskEditCardTemplate} from "./view/card-edit.js";
+import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
+import {generateTask} from "./mock/task.js";
+import {generateFilter} from "./mock/filter.js";
 
-const TASK_CARD_COUNT = 4;
+const TASK_CARD_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
 
 const tasks = new Array(TASK_CARD_COUNT).fill().map(generateTask);
+const {length: tasksLenght} = tasks;
 const editTaskTemplate = tasks[0];
+const filters = generateFilter(tasks);
 
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -19,12 +23,11 @@ const pageMainElement = document.querySelector(`.main`);
 const pageHeaderElement = pageMainElement.querySelector(`.main__control`);
 
 renderTemplate(pageHeaderElement, createPageMenuTemplate(), `beforeend`);
-renderTemplate(pageMainElement, createFilterTemplate(), `beforeend`);
+renderTemplate(pageMainElement, createFilterTemplate(filters), `beforeend`);
 renderTemplate(pageMainElement, createTaskBoardTemplate(), `beforeend`);
 
 const taskBoardElement = pageMainElement.querySelector(`.board`);
 const taskListElement = taskBoardElement.querySelector(`.board__tasks`);
-
 
 renderTemplate(taskListElement, createTaskEditCardTemplate(editTaskTemplate), `beforeend`);
 
@@ -32,4 +35,14 @@ for (let i = 1; i < TASK_CARD_COUNT; i++) {
   renderTemplate(taskListElement, createTaskCardTemplate(tasks[i]), `beforeend`);
 }
 
-renderTemplate(taskBoardElement, createLoadMoreButtonTemplate(), `beforeend`);
+if (tasksLenght > TASK_COUNT_PER_STEP) {
+  renderTemplate(taskBoardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = taskBoardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    alert(`Works!`);
+  });
+}
+
