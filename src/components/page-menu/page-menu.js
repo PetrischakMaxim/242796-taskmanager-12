53f383574ced2afbe289115921893562509cd841
Maksim Petrishchak <petrischak.maxim@gmail.com>
2020-08-13 17:47:16 +0300

    Переписал функцию меню на класс

--- src/components/page-menu/page-menu.js
+++ src/components/page-menu/page-menu.js
@@ -1,4 +1,6 @@
-export const createPageMenuTemplate = () => {
+import {createElement} from "../../utils.js";
+
+const createPageMenuTemplate = () => {
   return `<section class="control__btn-wrap">
           <input type="radio" name="control" id="control__new-task" class="control__input visually-hidden">
           <label for="control__new-task" class="control__label control__label--new-task">+ ADD NEW TASK</label>
@@ -8,3 +10,25 @@ export const createPageMenuTemplate = () => {
           <label for="control__statistic" class="control__label">STATISTICS</label>
         </section>`;
 };
+
+export default class PageMenu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createPageMenuTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
