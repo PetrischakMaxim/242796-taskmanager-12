8eafe38f1b40afa455a1b276610deda50e40dbe6
Maxim Petischak <petrischak.maxim@gmail.com>
2020-08-16 13:41:27 +0300

    Добавил унаследование простым компонентам

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,4 +1,4 @@
-import {createElement} from "../../utils/dom-utils.js";
+import AbstractView from "../abstract.js";
 
 const createMenuTemplate = () => {
   return `<section class="control__btn-wrap">
@@ -11,23 +11,10 @@ const createMenuTemplate = () => {
         </section>`;
 };
 
-export default class Menu {
-  constructor() {
-    this._element = null;
-  }
+export default class Menu extends AbstractView {
 
   getTemplate() {
     return createMenuTemplate();
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
 }
